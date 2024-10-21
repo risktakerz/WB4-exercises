a2bb83769f7b2a4e7677ecf2738339fb75380b74
risktakerz <paintkash@proton.me>
2024-10-21 11:07:37 -0400

    WB4-EXCERCISES

---
+++ demos/objects.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let person = {
+  first: "Shawn",
+  middle: "Corey",
+  last: "Carter",
+  age: 54,
+  alias: "Jay Z",
+  netWorth: 2500000000,
+};
+
+console.log(person.middle);
+
+function printPerson(person) {
+  console.log(person);
+}
+
+printPerson(person);
+
+function createWorker() {
+    let person = {
+        first: "Shawn",
+        middle: "Corey",
+        last: "Carter",
+        age: 28,
+        alias: "Jay Z",
+        netWorth: 2500000000,
+    }
+}
